8d5186072d986e5a77f9af4cad266391a4a9458c
RafaelPaez04 <144265875+RafaelPaez04@users.noreply.github.com>
2023-11-09 10:53:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,118 +1,183 @@
 import pygame
 import random
+import subprocess
 
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((1600, 600))
 pygame.display.set_caption('T-Rex Runner')
 
-player_image = pygame.image.load("sprites_jogo/player.png")
-player_image = pygame.transform.scale(player_image, (60, 60))
-ground_image = pygame.image.load("sprites_jogo/ground.png")
-ground_image = pygame.transform.scale(ground_image, (2600, 190))
-obstacle_image = pygame.image.load("sprites_jogo/obstacle.png")
-obstacle_image = pygame.transform.scale(obstacle_image, (70, 70))
-tsunami_image = pygame.image.load("sprites_jogo/tsunami.png")
-tsunami_image = pygame.transform.scale(tsunami_image, (2200, 300))
-background = pygame.image.load("sprites_jogo/fundo.png")
-background = pygame.transform.scale(background, (3200, 430))
+menu_principal = True
+menu_jogar = False
+menu_rank = False
+menu_configs = False
+menu_creditos = False
 
-
-player_x = 400
-player_y = 400
-player_y_change = 0
-player_y_limit = 360
-
-ground_x = 0
-ground_speed = 7
-obstacle_speed = 7
-tsunami_x = -1750
-tsunami_speed = 10
-background_speed = 3
-background_x = 0
-
-
-obstacles = []
-
-def check_collision(player_x, player_y, obstacles):
-    player_rect = pygame.Rect(player_x, player_y, player_image.get_width(), player_image.get_height())
-    for obstacle in obstacles:
-        if player_rect.colliderect(obstacle):
-            return True
-    return False
-
-game_over = False
-game_over_text = None
-
-clock = pygame.time.Clock()
-
-while True:
-    gameDisplay.fill((112,154,209))
+while menu_principal:
+    gameDisplay.fill((112, 154, 209))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-
-    if not game_over:
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and player_y == player_y_limit:
-            player_y_change = -18
-        if player_y < player_y_limit:
-            player_y_change += 1.2
-        player_y += player_y_change
-
-        if player_y > player_y_limit:
-            player_y = player_y_limit
-
-        ground_x -= ground_speed
-        if ground_x <= -1600:
-            ground_x = 0
             
-        background_x -= background_speed
-        if background_x <= -2600:
-            background_x = 1600
+    if menu_principal:
 
-        if random.randint(0, 500) < 5:
-            obstacles.append(pygame.Rect(1600, 350, obstacle_image.get_width(), obstacle_image.get_height()))
+        font = pygame.font.Font(None, 36)
+        boasvindas_text = font.render("Menu principal", True, (255, 255, 255))
+        jogar_text = font.render("1. Jogar", True, (255, 255, 255))
+        rank_text = font.render("2. Ranking", True, (255, 255, 255))
+        configs_text = font.render("3. Configs", True, (255, 255, 255))
+        creditos_text = font.render("4. Créditos", True, (255, 255, 255))
 
-        for obstacle in obstacles:
-            obstacle.x -= obstacle_speed
+        gameDisplay.blit(boasvindas_text, (575, 150))
+        gameDisplay.blit(jogar_text, (600, 200))
+        gameDisplay.blit(rank_text, (600, 250))
+        gameDisplay.blit(configs_text, (600, 300))
+        gameDisplay.blit(creditos_text, (600, 350))
 
-        if check_collision(player_x, player_y, obstacles):
-            game_over = True
-            game_over_text = pygame.font.Font(None, 70).render("Game Over", True, (255, 0, 0))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_1]:
+            menu_principal = False
+            menu_jogar = True
+        elif keys[pygame.K_2]:
+            menu_principal = False
+            menu_rank = True
+        elif keys[pygame.K_3]:
+            menu_principal = False
+            menu_configs = True
+        elif keys[pygame.K_4]:
+            menu_principal = False
+            menu_creditos = True
 
-    else:
-        tsunami_x += tsunami_speed
-        if tsunami_x > 5:
-            tsunami_speed = 0
-        
-  
+        while menu_jogar:
+            gameDisplay.fill((112, 154, 209))
 
-    
-    gameDisplay.blit(background, (background_x, 0))
-    gameDisplay.blit(ground_image, (ground_x, 410))
-    gameDisplay.blit(ground_image, (ground_x + 600, 410))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
-    
-    
-    gameDisplay.blit(player_image, (player_x, player_y))
-    
+            font = pygame.font.Font(None, 36)
+            jogos_text = font.render("Modos de jogo:", True, (255, 255, 255))
+            tsunami_text = font.render("1. Tsunami", True, (255, 255, 255))
+            tornado_text = font.render("2. Tornado", True, (255, 255, 255))
+            meteoro_text = font.render("3. Meteoro", True, (255, 255, 255))
+            iniciar_jogo_text = font.render("Digite uma das opções acima e pressione SPACE para iniciar", True, (255, 255, 255))
+            retornar = font.render("Pressione Esc para retornar", True, (255, 255, 255))
 
-    for obstacle in obstacles:
-        gameDisplay.blit(obstacle_image, (obstacle.x, obstacle.y))
+            gameDisplay.blit(jogos_text, (575, 150))
+            gameDisplay.blit(tsunami_text, (600, 200))
+            gameDisplay.blit(tornado_text, (600, 250))
+            gameDisplay.blit(meteoro_text, (600, 300))
+            gameDisplay.blit(iniciar_jogo_text, (600, 350))
+            gameDisplay.blit(retornar, (575, 450))
 
-    gameDisplay.blit(tsunami_image, (tsunami_x, 120))
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_1]:
+                selected_game = "fase_1.py"
+            elif keys[pygame.K_2]:
+                selected_game = "fase_2.py"
+            elif keys[pygame.K_3]:
+                selected_game = "fase_3.py"
+            elif keys[pygame.K_ESCAPE]:
+                menu_jogar = False
+                menu_principal = True
 
+            if selected_game and keys[pygame.K_SPACE]:
+                subprocess.Popen(["python", selected_game])
+                selected_game = None 
 
+            pygame.display.update()
 
-    if game_over_text:
-        gameDisplay.blit(game_over_text, (700, 200))
+        while menu_rank:
+            gameDisplay.fill((112, 154, 209))
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            font = pygame.font.Font(None, 36)
 
-    ground_speed += 0.0095
-    obstacle_speed += 0.0095
+            def read_scores():
+                with open("scores1.txt", "r") as file:
+                    scores = [line.strip().split(": ") for line in file]
+
+                scores.sort(key=lambda x: int(x[1]), reverse=True)
+                return scores
+
+            def show_ranking():
+                gameDisplay.fill((0, 0, 0))
+                scores = read_scores()
+                y = 100 
+                for name, score in scores[:2]: 
+                    score_text = font.render(f"{name}: {score}", True, (255, 255, 255))
+                    gameDisplay.blit(score_text, (100, y))
+                    y += 40
+
+            show_ranking()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                menu_rank = False
+                menu_principal = True
+
+            pygame.display.update()
+
+        while menu_configs:
+            gameDisplay.fill((112, 154, 209))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            font = pygame.font.Font(None, 36)
+            creditos_text = font.render("Controles: ", True, (255, 255, 255))
+            orientacoes_text = font.render("Pressione SPACE para pular", True, (255, 255, 255))
+            retornar = font.render("Pressione Esc para retornar", True, (255, 255, 255))
+
+            gameDisplay.blit(creditos_text, (575, 100))
+            gameDisplay.blit(orientacoes_text, (600, 150))
+            gameDisplay.blit(retornar, (575, 250))
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                menu_configs = False
+                menu_principal = True
+
+            pygame.display.update()
+
+        while menu_creditos:
+            gameDisplay.fill((112, 154, 209))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    
+            font = pygame.font.Font(None, 36)
+            creditos_text = font.render("Créditos", True, (255, 255, 255))
+            desenvolvedores_text = font.render("Desenvolvido por:", True, (255, 255, 255))
+            nome_desenvolvedor_1 = font.render("Nome do Desenvolvedor 1", True, (255, 255, 255))
+            nome_desenvolvedor_2 = font.render("Nome do Desenvolvedor 2", True, (255, 255, 255))
+            retornar = font.render("Pressione Esc para retornar", True, (255, 255, 255))
+
+            gameDisplay.blit(creditos_text, (575, 100))
+            gameDisplay.blit(desenvolvedores_text, (600, 150))
+            gameDisplay.blit(nome_desenvolvedor_1, (600, 200))
+            gameDisplay.blit(nome_desenvolvedor_2, (600, 250))
+            gameDisplay.blit(retornar, (575, 350))
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                menu_creditos = False
+                menu_principal = True
+
+            pygame.display.update()
 
     pygame.display.update()
-    clock.tick(60)
+
+pygame.quit()
+quit()
