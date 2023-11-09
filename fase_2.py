d180892caf3557dfcfcc815874e13c8c7c966def
RafaelPaez04 <144265875+RafaelPaez04@users.noreply.github.com>
2023-11-09 10:54:29 -0300

    Update fase_2.py

--- fase_2.py
+++ fase_2.py
@@ -1,16 +1,17 @@
 import pygame
 import random
+import sys
 
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((1600, 600))
 pygame.display.set_caption('T-Rex Runner')
 
-player_image = pygame.image.load("sprites_jogo/player.png")
+player_image = pygame.image.load("sprites_jogo/player.png").convert()
 player_image = pygame.transform.scale(player_image, (60, 60))
-ground_image = pygame.image.load("sprites_jogo/ground.png")
+ground_image = pygame.image.load("sprites_jogo/ground.png").convert()
 ground_image = pygame.transform.scale(ground_image, (2600, 190))
-obstacle_image = pygame.image.load("sprites_jogo/obstacle.png")
+obstacle_image = pygame.image.load("sprites_jogo/obstacle.png").convert()
 obstacle_image = pygame.transform.scale(obstacle_image, (70, 70))
 
 obstacle_image_jump = pygame.image.load("sprites_jogo/tire.png")
@@ -18,8 +19,16 @@ obstacle_image_jump = pygame.transform.scale(obstacle_image_jump, (50, 50))
 
 tsunami_image = pygame.image.load("sprites_jogo/tornado.png")
 tsunami_image = pygame.transform.scale(tsunami_image, (500, 300))
-background = pygame.image.load("sprites_jogo/fundo_2.png")
+background = pygame.image.load("sprites_jogo/fundo_2.png").convert()
 background = pygame.transform.scale(background, (3200, 430))
+
+pygame.mixer.init()
+pygame.mixer.music.load("/Users/rafael/Downloads/musica.mp3")
+pygame.mixer.music.set_volume(1.0)
+pygame.mixer.music.play(1)
+music_playing = True
+
+
 
 player_x = 400
 player_y = 400
@@ -33,12 +42,12 @@ tsunami_x = -100
 tsunami_speed = 10
 background_speed = 3
 background_x = 0
+score = 0
 
 obstacles = []
 obstacles_jump = []
 
-
-obstacles_jump_y = 350  
+obstacles_jump_y = 360
 obstacles_jump_speed = 2  
 obstacles_jump_direction = 1  
 
@@ -52,7 +61,14 @@ def check_collision(player_x, player_y, obstacles):
 game_over = False
 game_over_text = None
 
+font = pygame.font.Font(None, 70)
+game_over_font = pygame.font.Font(None, 70)
+
 clock = pygame.time.Clock()
+
+def save_score(name, score):
+    with open("scores2.txt", "a") as file:
+        file.write(f"{name}: {score}\n")
 
 while True:
     gameDisplay.fill((127,127,127))
@@ -63,11 +79,24 @@ while True:
             quit()
 
     if not game_over:
+
+        score_text = font.render(f"Score: {int(score)}", True, (255, 0, 0))
+        score += 0.095
+
         keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_m] and music_playing == True:
+            pygame.mixer.music.stop()
+            music_playing = False
+
+        if keys[pygame.K_u] and music_playing == False :
+            pygame.mixer.music.play()
+            music_playing = True
+
         if keys[pygame.K_SPACE] and player_y == player_y_limit:
             player_y_change = -18
         if player_y < player_y_limit:
-            player_y_change += 1.2
+            player_y_change += 0.9
         player_y += player_y_change
 
         if player_y > player_y_limit:
@@ -87,11 +116,11 @@ while True:
         if random.randint(0, 500) < 2:
             obstacles_jump.append(pygame.Rect(1600, obstacles_jump_y, obstacle_image_jump.get_width(), obstacle_image_jump.get_height()))
 
-        for obstacle in obstacles:
+        for i, obstacle in enumerate(obstacles):
             obstacle.x -= obstacle_speed
-            
-        for obstacle in obstacles_jump:
-            obstacle.x -= obstacle_speed
+
+        for i, obstacle in enumerate(obstacles_jump):
+            obstacle.x -= (obstacle_speed + 2)
             
         if check_collision(player_x, player_y, obstacles):
             game_over = True
@@ -106,6 +135,29 @@ while True:
         if tsunami_x > 1000:
             tsunami_speed = 0
 
+        name = ""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.unicode.isalpha():
+                        name += event.unicode
+                    elif event.key == pygame.K_BACKSPACE:
+                        name = name[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        save_score(name, int(score))
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            block = font.render(f"Enter Name: {name}", True, (255, 255, 255))
+            game_over = True
+            game_over_text = pygame.font.Font(None, 70).render("Game Over", True, (255, 0, 0))
+            gameDisplay.blit(game_over_text, (700, 200))
+            gameDisplay.blit(block, (700, 300))
+            pygame.display.update()
+
     obstacles_jump_y += obstacles_jump_speed * obstacles_jump_direction
     if obstacles_jump_y < 260:
         obstacles_jump_direction = 1
@@ -116,6 +168,7 @@ while True:
     gameDisplay.blit(ground_image, (ground_x, 410))
     gameDisplay.blit(ground_image, (ground_x + 600, 410))
     gameDisplay.blit(player_image, (player_x, player_y))
+    gameDisplay.blit(score_text, (150, 50))
 
     for obstacle in obstacles:
         gameDisplay.blit(obstacle_image, (obstacle.x, obstacle.y))
