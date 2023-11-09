646d87fe1ca35b30f1c713f049856891520e8d83
RafaelPaez04 <144265875+RafaelPaez04@users.noreply.github.com>
2023-11-09 15:34:42 -0300

    Update fase_1.py

--- fase_1.py
+++ fase_1.py
@@ -89,8 +89,11 @@ while True:
         if keys[pygame.K_SPACE] and player_y == player_y_limit:
             player_y_change = -18
         if player_y < player_y_limit:
-            player_y_change += 1.2
+            player_y_change += 0.9
         player_y += player_y_change
+
+        if keys[pygame.K_s] and player_y < player_y_limit:
+            player_y_change = +20
 
         if player_y > player_y_limit:
             player_y = player_y_limit
