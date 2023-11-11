e70e709f67bcae4c2b42e7b10fc567358d0604c3
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-11-11 14:33:53 -0300

    atualização e otimização

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import subprocess
+import os
 
 pygame.init()
 
@@ -15,12 +16,12 @@ menu_creditos = False
 
 while menu_principal:
     gameDisplay.fill((112, 154, 209))
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-            
+
     if menu_principal:
 
         font = pygame.font.Font(None, 36)
@@ -41,6 +42,17 @@ while menu_principal:
             menu_principal = False
             menu_jogar = True
         elif keys[pygame.K_2]:
+            def read_scores():
+                if os.path.exists("scores.txt"):
+                    with open("scores.txt", "r") as file:
+                        scores = [line.strip().split(": ") for line in file]
+
+                    scores.sort(key=lambda x: int(x[1]), reverse=True)
+                    return scores
+                else:
+                    return []
+
+
             menu_principal = False
             menu_rank = True
         elif keys[pygame.K_3]:
@@ -63,8 +75,10 @@ while menu_principal:
             tsunami_text = font.render("1. Tsunami", True, (255, 255, 255))
             tornado_text = font.render("2. Tornado", True, (255, 255, 255))
             meteoro_text = font.render("3. Meteoro", True, (255, 255, 255))
-            iniciar_jogo_text = font.render("Digite uma das opções acima e pressione SPACE para iniciar", True, (255, 255, 255))
+            iniciar_jogo_text = font.render("Digite uma das opções acima e pressione SPACE para iniciar", True,
+                                            (255, 255, 255))
             retornar = font.render("Pressione Esc para retornar", True, (255, 255, 255))
+            selected = font.render("•", True, (255, 255, 255))
 
             gameDisplay.blit(jogos_text, (575, 150))
             gameDisplay.blit(tsunami_text, (600, 200))
@@ -75,10 +89,13 @@ while menu_principal:
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_1]:
+                gameDisplay.blit(selected, (570, 200))
                 selected_game = "fase_1.py"
             elif keys[pygame.K_2]:
+                gameDisplay.blit(selected, (570, 250))
                 selected_game = "fase_2.py"
             elif keys[pygame.K_3]:
+                gameDisplay.blit(selected, (570, 300))
                 selected_game = "fase_3.py"
             elif keys[pygame.K_ESCAPE]:
                 menu_jogar = False
@@ -86,7 +103,7 @@ while menu_principal:
 
             if selected_game and keys[pygame.K_SPACE]:
                 subprocess.Popen(["python", selected_game])
-                selected_game = None 
+                selected_game = None
 
             pygame.display.update()
 
@@ -97,24 +114,30 @@ while menu_principal:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                    
+
             font = pygame.font.Font(None, 36)
 
-            def read_scores():
-                with open("scores1.txt", "r") as file:
-                    scores = [line.strip().split(": ") for line in file]
 
-                scores.sort(key=lambda x: int(x[1]), reverse=True)
-                return scores
+            def read_scores():
+                if os.path.exists("scores.txt"):
+                    with open("scores.txt", "r") as file:
+                        scores = [line.strip().split(": ") for line in file]
+
+                    scores.sort(key=lambda x: int(x[1]), reverse=True)
+                    return scores
+                else:
+                    return []
+
 
             def show_ranking():
                 gameDisplay.fill((0, 0, 0))
                 scores = read_scores()
-                y = 100 
-                for name, score in scores[:2]: 
+                y = 100
+                for name, score in scores[:5]:
                     score_text = font.render(f"{name}: {score}", True, (255, 255, 255))
                     gameDisplay.blit(score_text, (100, y))
                     y += 40
+
 
             show_ranking()
 
@@ -132,15 +155,21 @@ while menu_principal:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                    
+
             font = pygame.font.Font(None, 36)
             creditos_text = font.render("Controles: ", True, (255, 255, 255))
             orientacoes_text = font.render("Pressione SPACE para pular", True, (255, 255, 255))
+            orientacoes_text2 = font.render("Pressione S para cair mais rapido", True, (255, 255, 255))
+            orientacoes_text3 = font.render("Pressione M para silenciar o jogo e U para retomar a música", True,
+                                            (255, 255, 255))
+
             retornar = font.render("Pressione Esc para retornar", True, (255, 255, 255))
 
             gameDisplay.blit(creditos_text, (575, 100))
             gameDisplay.blit(orientacoes_text, (600, 150))
-            gameDisplay.blit(retornar, (575, 250))
+            gameDisplay.blit(orientacoes_text2, (600, 200))
+            gameDisplay.blit(orientacoes_text3, (600, 250))
+            gameDisplay.blit(retornar, (575, 350))
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE]:
@@ -156,7 +185,7 @@ while menu_principal:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-                    
+
             font = pygame.font.Font(None, 36)
             creditos_text = font.render("Créditos", True, (255, 255, 255))
             desenvolvedores_text = font.render("Desenvolvido por:", True, (255, 255, 255))
